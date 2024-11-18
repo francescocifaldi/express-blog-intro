1370c303c1dbd0db929347a1075159716d03f6e6
francescocifaldi <francesco.cifaldi@gmail.com>
2024-11-18 15:26:10 +0100

    ADD exercise

---
+++ app.js
@@ -0,0 +1,52 @@
+// console.log('Hello world')
+
+const express = require ('express')
+const app = express()
+const port = 3000
+
+const posts = [
+    {
+        title: "Ciambellone",
+        content: "la mia ricetta",
+        image: "/images/ciambellone.jpeg", 
+        tags: ["dolce","ciambella"],
+    },
+    {
+        title: "Cracker alla barbabietola",
+        content: "la mia ricetta",
+        image: "images/cracker_barbabietola.jpeg", 
+        tags: ["dolce","cracker"],
+    },
+    {
+        title: "Pane fritto dolce",
+        content: "la mia ricetta",
+        image: "images/pane_fritto_dolce.jpeg", 
+        tags: ["dolce","fritto"],
+    },
+    {
+        title: "Pasta alla barbabietola",
+        content: "la mia ricetta",
+        image: "images/pasta_barbabietola.jpeg", 
+        tags: ["salato","pasta"],
+    },
+    {
+        title: "Torta paesana",
+        content: "la mia ricetta",
+        image: "images/torta_paesana.jpeg", 
+        tags: ["dolce","torta"],
+    }
+]
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
+
+app.get('/bacheca', (req, res) => {
+    res.json(posts)
+})
+
+app.listen(port, () => {
+
+})
